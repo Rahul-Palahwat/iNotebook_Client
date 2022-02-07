039714f51bfa48f5d6c6dfed21192bf54620fc9b
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-02-07 09:39:10 +0530

    Dont know the commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -22,6 +22,8 @@ router.post('/createuser',[
     }
 
     // ab yen data ko save krne ke liye h and check weather the user alreday exists with the same email
+    // try and catch is liye h ki mano kbhi bhi koi dikkat aa gyi hmare database me toh ek error hme mil jaye 
+    try {
     let user=await User.findOne({email: req.body.email});
     if(user){
       return res.status(400).json({error: "sorry email already exits"});
@@ -32,6 +34,10 @@ router.post('/createuser',[
         password: req.body.password,
       })
     res.json({user});
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+  }
 
     // res.send(req.body);
 });
