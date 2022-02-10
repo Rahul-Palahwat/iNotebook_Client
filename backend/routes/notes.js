a9c04d5c70f1b2beae4dccd4d436eb70dc90834c
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-02-10 13:50:54 +0530

    Lect 53 Updating an existing Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -11,7 +11,7 @@ var fetchuser = require("../middleware/fetchuser");
 // this is for validation of input
 const { body, validationResult } = require("express-validator");
 
-// Route 1: Get all the notes of the logged in user using : GET "/api/auth/fetchallnotes" Login required
+// Route 1: Get all the notes of the logged in user using : GET "/api/notes/fetchallnotes" Login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     // req.user middleware se aayega
@@ -23,7 +23,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-// Route 2: Add a new note using post method  : POST "/api/auth/addnote" Login required
+// Route 2: Add a new note using post method  : POST "/api/notes/addnote" Login required
 router.post(
   "/addnote",
   fetchuser,
@@ -55,6 +55,40 @@ router.post(
     }
   }
 );
+
+
+// Route 3: Update an existing note using post method  : POST "/api/notes/updatenote" Login required
+router.put(
+    "/updatenote/:id",
+    fetchuser,
+    async (req, res) => {
+        const {title,description,tag}=req.body;
+        //now we will create a new Note object
+        const newNote={};
+        if(title){
+            newNote.title=title;
+        }
+        if(description){
+            newNote.description=description;
+        }
+        if(tag){
+            newNote.tag=tag;
+        }
+
+        //find the node to be updated and update it
+        // ab yen check krenge ki kya yen whi user h jiska yen note h 
+        let note=await Note.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not found");
+        }
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("Not allowed");
+        }
+        note=await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+        res.json(note);
+
+
+    })
 
 // ab yha pr router ko export krenge
 module.exports = router;
