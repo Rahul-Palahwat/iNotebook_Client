8124e739dcbcaffa73b74e6401669cd88013bca8
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-02-12 11:30:53 +0530

    Lect 59 using useLocation Hook to get the location of our page and to active the same anchor tag in navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,12 @@
-import React,{ useContext, useEffect } from 'react'
+import React from 'react'
 
-//yha pr context import kiya h jisse ki hmm 
-import noteContext from '../context/notes/noteContext'
 
 
 export default function About() {
-  const a=useContext(noteContext);
-  useEffect(()=>{
-    a.update();
-  },[])
+  
   return (
     <div>
-      This is About {a.state.name} and he is in {a.state.class}
+      This is About page
     </div>
   )
 }
